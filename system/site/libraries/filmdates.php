fd4c2684c7c888dccd5f22c953a438fe16dcac08
Chris Harvey <chris@chrisnharvey.com>
2016-02-16 21:13:57 +0000

    Set a Mozilla user agent to Mozilla for Filmdates API

--- system/site/libraries/filmdates.php
+++ system/site/libraries/filmdates.php
@@ -13,6 +13,8 @@ class Filmdates
 			return $call;
 		}
 
+
+
 		return FALSE;
 	}
 	
@@ -23,6 +25,7 @@ class Filmdates
 
 		curl_setopt($ch, CURLOPT_URL, $this->url);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322)');
 		
 		$response = curl_exec($ch);
 
